1c981972fd8e5ff182a8fe683af22cb57dc1040d
Sebastien Do <sdo@arlo.com>
2023-03-01 11:42:14 -0800

    CMFW-17596: at_arlo_init in log_service.c (#108)
    * CMFW-17596: put arlo AT command init in log_service.c (#102)
    
    * CMFW-17596: make sure it builds for rtk (#107)

--- component/at_cmd/log_service.c
+++ component/at_cmd/log_service.c
@@ -44,6 +44,7 @@ extern void inic_ipc_mp_command(char *token, unsigned int cmd_len, int show_msg)
 extern int wext_private_command(const char *ifname, char *cmd, int show_msg);
 #endif
 #endif
+extern void at_ftl_init(void);
 #if defined(ESSENTIAL2)
 extern void at_arlo_init(void);
 #endif
